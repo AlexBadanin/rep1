c84ee29a6ac8950511652b75cc863fbf42c3bf16
AlexBadanin <sashabadanin147@gmail.com>
2024-09-19 19:40:29 +0300

    new file

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,5 +1,5 @@
 public class MyFirstApp {   
     public static void main(  String[] args ) {
-        System.out.println("Write once, run пролАА56рр");
+        System.out.println("Write once, run про");
     }
 }
